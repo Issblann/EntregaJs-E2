6a13b2d095ee6c9e2dc1d29540c276e441fb4d1e
issblann <anaisabelpp@outlook.com>
2022-12-07 14:24:28 -0500

    agrego generador de pizzas

---
+++ assets/main.js
@@ -0,0 +1,127 @@
+const numberInput = document.getElementById("numberInput");
+const submitInput = document.getElementById("submitInput");
+const form = document.getElementById("form");
+const container = document.getElementById("containerCards");
+
+const Pizzas = [
+  {
+    nombre: "Hawaiana",
+    id: 1,
+    ingredientes: ["Piña", "Jamon", "Queso"],
+    precio: 566,
+  },
+
+  {
+    nombre: "Jamon y Queso",
+    id: 2,
+    ingredientes: ["Jamon", "Queso"],
+    precio: 566,
+  },
+
+  {
+    nombre: "Mexicana",
+    id: 3,
+    ingredientes: ["Carne desmechada", "Nachos", "Queso"],
+    precio: 727,
+  },
+
+  {
+    nombre: "Samba",
+    id: 4,
+    ingredientes: ["Maduro", "Maiz", "Tocineta", "Jamon", "Queso"],
+    precio: 727,
+  },
+
+  {
+    nombre: "Vegetariana",
+    id: 5,
+    ingredientes: [
+      "Tomate",
+      "Oregano",
+      "Cebolla",
+      "Pimentón",
+      "Champiñon",
+      "Queso",
+    ],
+    precio: 889,
+  },
+
+  {
+    nombre: "Napolitana",
+    id: 6,
+    ingredientes: ["Tomate", "Oregano", "Queso"],
+    precio: 889,
+  },
+
+  {
+    nombre: "Atlantias Americana",
+    id: 7,
+    ingredientes: ["Salchicha", "Jamon", "Tocineta", "Queso"],
+    precio: 889,
+  },
+];
+
+// const capturaInput = (numberValue) => {
+//   numberValue = numberInput.value.trim();
+//   // container.textContent = `Este es el resultado ${numberValue}`;
+//   return numberValue;
+// };
+
+// console.log(capturaInput());
+
+const agarraPizza = () => {
+  const numberValue = numberInput.value.trim();
+
+  const findPizzas = Pizzas.find((pizza) => {
+    return pizza.id == numberValue;
+  });
+  if (isEmpty(numberValue)) {
+    container.classList.remove("hidden");
+    container.innerHTML = "<h3> Ingresa algun ID </h3>";
+  } else if (!findPizzas) {
+    container.innerHTML = "<h3> Ingresa algun ID valido </h3>";
+    container.classList.remove("hidden");
+  }
+  if (findPizzas) {
+    container.classList.remove("hidden");
+    container.innerHTML = `<div>
+    <h2 class="titulo-pizza">${findPizzas.nombre}</h2>
+    <h3 class="precio">$${findPizzas.precio}</h3>
+  </div>
+  <div><i class="fa-solid fa-pizza-slice"></i></div>`;
+  }
+};
+
+const isEmpty = (value) => !value.length;
+
+// const funcionIdPizzas = () => {
+//   const findIdPizzas = Pizzas.find((pizza) => {
+//     return pizza.id == numberValue;
+//   });
+
+//   // filterIdPizzas.forEach((pizza) => {
+//   //   pizza.id;
+//   //   console.log(pizza.id);
+//   // });
+//   return findIdPizzas;
+// };
+
+// funcionIdPizzas(numberValue);
+
+// const funcionNamePizzas = () => {
+//   const findNamePizzas = Pizzas.find((pizza) => {
+//     return pizza.nombre;
+//   });
+//   // findNamePizzas.forEach((pizza) => {
+//   //   pizza.nombre;
+//   //   console.log(pizza.nombre);
+//   // });
+//   return findNamePizzas;
+// };
+
+// funcionNamePizzas();
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  agarraPizza();
+});
